8b71ecefd44fa85844bb89f532aad09a3dfaad3e
allenchen <allenchen614@gmail.com>
2022-06-26 20:19:57 +0800

    init

---
+++ train_lsan_on_aapd.py
@@ -0,0 +1,165 @@
+import numpy as np
+from mxnet.contrib import text
+import torch.utils.data as data_utils
+import torch
+import utils.lsan_util as util
+from models.lsan import StructuredSelfAttention
+from tqdm import tqdm
+
+## 加载配置
+config = util.read_config("files/lsan/config.yml")
+if config.GPU:
+    torch.cuda.set_device(0)
+batch_size = config.batch_size
+
+## 导入数据
+def load_data():
+    # 修改路径
+    X_tst = np.load("data/AAPD/X_test.npy")
+    X_trn = np.load("data/AAPD/X_train.npy")
+    Y_trn = np.load("data/AAPD/y_train.npy")
+    Y_tst = np.load("data/AAPD/y_test.npy")
+
+    return (X_trn, Y_trn), (X_tst, Y_tst)
+
+train_data, test_data = load_data()
+
+## 数据生成器
+
+train_data = data_utils.TensorDataset(torch.from_numpy(train_data[0]).type(torch.LongTensor),
+                                          torch.from_numpy(train_data[1]).type(torch.LongTensor))
+test_data = data_utils.TensorDataset(torch.from_numpy(test_data[0]).type(torch.LongTensor),
+                                        torch.from_numpy(test_data[1]).type(torch.LongTensor))
+train_loader = data_utils.DataLoader(train_data, batch_size, shuffle=True, drop_last=True)
+test_loader = data_utils.DataLoader(test_data, batch_size, drop_last=True)
+
+
+## 定义模型
+word_embed = text.embedding.CustomEmbedding('files/lsan/word_embed.txt').idx_to_vec.asnumpy()
+label_embed = np.load("files/lsan/label_embed.npy")
+word_embed = torch.from_numpy(word_embed).float()
+label_embed = torch.from_numpy(label_embed).float()
+
+label_num = label_embed.shape[0]
+
+model = StructuredSelfAttention(
+    batch_size=batch_size, 
+    lstm_hid_dim=config['lstm_hidden_dimension'],
+    d_a=config["d_a"], 
+    n_classes=label_num, 
+    label_embed=label_embed,
+    embeddings=word_embed
+)
+
+loss = torch.nn.BCELoss()
+opt = torch.optim.Adam(model.parameters(), lr=0.001, betas=(0.9, 0.99))
+
+model_file = 'output/lsan_aapd.best_model'
+
+class Trainer(object):
+    def __init__(self, model, criterion, optimizer, train_dataset, test_dataset, epochs, use_cuda = False):
+        self.model = model
+        self.criterion = criterion
+        self.optimizer = optimizer
+        self.train_dataset = train_dataset
+        self.test_dataset = test_dataset
+        self.use_cuda = use_cuda
+        self.epochs = epochs
+        self.n_epoch = 1
+        self.best_eval_score = .0
+
+        if self.use_cuda:
+            self.model = self.model.cuda()
+    
+    def run(self):
+        for i in range(self.epochs):
+            self.train()
+            self.evaluation()
+    
+    def train(self):
+        self.model.train()
+
+        print("Running EPOCH",self.n_epoch)
+        train_loss = []
+        prec_k = []
+        ndcg_k = []
+        for i, (x, y) in enumerate(tqdm(self.train_dataset)):
+            # x, y = train[0].cuda(), train[1].cuda()
+            if self.use_cuda:
+                x = x.cuda()
+                y = y.cuda()
+            # 前向传播
+            out = self.model(x)
+            loss = self.criterion(out, y.float()) / x.shape[0]
+            # 反向传播，更新参数
+            loss.backward()
+            self.optimizer.zero_grad()
+            self.optimizer.step()
+            # 计算指标
+            labels_cpu = y.data.cpu().float()
+            pred_cpu = out.data.cpu()
+            prec = util.precision_k(labels_cpu.numpy(), pred_cpu.numpy(), 5)
+            ndcg = util.Ndcg_k(labels_cpu.numpy(), pred_cpu.numpy(), 5)
+
+            prec_k.append(prec)
+            ndcg_k.append(ndcg)
+            train_loss.append(float(loss))
+        
+        # 打印日志
+        avg_loss = np.mean(train_loss)
+        epoch_prec = np.array(prec_k).mean(axis=0)
+        epoch_ndcg = np.array(ndcg_k).mean(axis=0)
+        print("epoch %2d train end : avg_loss = %.4f" % (self.n_epoch, avg_loss))
+        print("precision@1 : %.4f , precision@3 : %.4f , precision@5 : %.4f " % (epoch_prec[0], epoch_prec[2], epoch_prec[4]))
+        print("ndcg@1 : %.4f , ndcg@3 : %.4f , ndcg@5 : %.4f " % (epoch_ndcg[0], epoch_ndcg[2], epoch_ndcg[4]))
+        self.n_epoch += 1
+    
+    def evaluation(self):
+        self.model.eval()
+
+        test_acc_k = []
+        test_loss = []
+        test_ndcg_k = []
+        with torch.no_grad(): # 或者@torch.no_grad() 被他们包裹的代码块不需要计算梯度， 也不需要反向传播
+            for i, (x, y) in enumerate(self.test_dataset):
+                if self.use_cuda:
+                    x = x.cuda()
+                    y = y.cuda()
+                out = self.model(x)
+                loss = self.criterion(out, y.float()) / x.shape[0]
+
+                labels_cpu = y.data.cpu().float()
+                pred_cpu = out.data.cpu()
+
+                prec = util.precision_k(labels_cpu.numpy(), pred_cpu.numpy(), 5)
+                ndcg = util.Ndcg_k(labels_cpu.numpy(), pred_cpu.numpy(), 5)
+
+                test_acc_k.append(prec)
+                test_ndcg_k.append(ndcg)
+                test_loss.append(float(loss))
+
+        avg_test_loss = np.mean(test_loss)
+        test_prec = np.array(test_acc_k).mean(axis=0)
+        test_ndcg = np.array(test_ndcg_k).mean(axis=0)
+        print("epoch %2d test end : avg_loss = %.4f" % (self.n_epoch, avg_test_loss))
+        print("precision@1 : %.4f , precision@3 : %.4f , precision@5 : %.4f " % (
+        test_prec[0], test_prec[2], test_prec[4]))
+        print("ndcg@1 : %.4f , ndcg@3 : %.4f , ndcg@5 : %.4f " % (test_ndcg[0], test_ndcg[2], test_ndcg[4]))
+        
+        if test_prec[0] > self.best_eval_score:
+            self.model.save(model_file)
+
+
+if __name__=='__main__':
+    Trainer(
+        model, 
+        criterion=loss,
+        optimizer=opt,
+        train_dataset=train_loader, 
+        test_dataset=test_loader,
+        epochs=10,
+        use_cuda=config.GPU
+    ).run()
+    
+else:
+    model.load_state_dict(torch.load(model_file))
---
+++ utils/lsan_util.py
@@ -0,0 +1,151 @@
+import os
+import csv
+import codecs
+import yaml
+import time
+import numpy as np
+
+from sklearn import metrics
+
+
+class AttrDict(dict):
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
+
+
+def read_config(path):
+
+    return AttrDict(yaml.load(open(path, 'r')))
+
+
+def read_datas(filename, trans_to_num=False):
+    lines = open(filename, 'r').readlines()
+    lines = list(map(lambda x: x.split(), lines))
+    if trans_to_num:
+        lines = [list(map(int, line)) for line in lines]
+    return lines
+
+
+def save_datas(data, filename, trans_to_str=False):
+    if trans_to_str:
+        data = [list(map(str, line)) for line in data]
+    lines = list(map(lambda x: " ".join(x), data))
+    with open(filename, 'w') as f:
+        f.write("\n".join(lines))
+
+
+def logging(file):
+    def write_log(s):
+        print(s, end='')
+        with open(file, 'a') as f:
+            f.write(s)
+    return write_log
+
+
+def logging_csv(file):
+    def write_csv(s):
+        with open(file, 'a', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(s)
+    return write_csv
+
+
+def format_time(t):
+    return time.strftime("%Y-%m-%d-%H:%M:%S", t)
+
+
+def eval_metrics(reference, candidate, label_dict, log_path):
+    ref_dir = log_path + 'reference/'
+    cand_dir = log_path + 'candidate/'
+    if not os.path.exists(ref_dir):
+        os.mkdir(ref_dir)
+    if not os.path.exists(cand_dir):
+        os.mkdir(cand_dir)
+    ref_file = ref_dir+'reference'
+    cand_file = cand_dir+'candidate'
+
+    for i in range(len(reference)):
+        with codecs.open(ref_file+str(i), 'w', 'utf-8') as f:
+            f.write("".join(reference[i])+'\n')
+        with codecs.open(cand_file+str(i), 'w', 'utf-8') as f:
+            f.write("".join(candidate[i])+'\n')
+        
+    def make_label(l, label_dict):
+        length = len(label_dict)
+        result = np.zeros(length)
+        indices = [label_dict.get(label.strip().lower(), 0) for label in l]
+        result[indices] = 1
+        return result
+
+    def prepare_label(y_list, y_pre_list, label_dict):
+        reference = np.array([make_label(y, label_dict) for y in y_list])
+        candidate = np.array([make_label(y_pre, label_dict) for y_pre in y_pre_list])
+        return reference, candidate
+
+    def get_metrics(y, y_pre):
+        hamming_loss = metrics.hamming_loss(y, y_pre)
+        macro_f1 = metrics.f1_score(y, y_pre, average='macro')
+        macro_precision = metrics.precision_score(y, y_pre, average='macro')
+        macro_recall = metrics.recall_score(y, y_pre, average='macro')
+        micro_f1 = metrics.f1_score(y, y_pre, average='micro')
+        micro_precision = metrics.precision_score(y, y_pre, average='micro')
+        micro_recall = metrics.recall_score(y, y_pre, average='micro')
+        return hamming_loss, macro_f1, macro_precision, macro_recall, micro_f1, micro_precision, micro_recall
+
+
+    y, y_pre = prepare_label(reference, candidate, label_dict)
+    hamming_loss, macro_f1, macro_precision, macro_recall, micro_f1, micro_precision, micro_recall = get_metrics(y, y_pre)
+    return {'hamming_loss': hamming_loss, 
+            'macro_f1': macro_f1,
+            'macro_precision': macro_precision, 
+            'macro_recall': macro_recall,
+            'micro_f1': micro_f1,
+            'micro_precision': micro_precision, 
+            'micro_recall': micro_recall}
+
+
+def precision_k(true_mat, score_mat, k):
+    p = np.zeros((k, 1))
+    rank_mat = np.argsort(score_mat)
+    backup = np.copy(score_mat)
+    for k in range(k):
+        score_mat = np.copy(backup)
+        for i in range(rank_mat.shape[0]):
+            score_mat[i][rank_mat[i, :-(k + 1)]] = 0
+        score_mat = np.ceil(score_mat)
+        #         kk = np.argwhere(score_mat>0)
+        mat = np.multiply(score_mat, true_mat)
+        #         print("mat",mat)
+        num = np.sum(mat, axis=1)
+        p[k] = np.mean(num / (k + 1))
+    return np.around(p, decimals=4)
+
+def Ndcg_k(true_mat, score_mat, k):
+    res = np.zeros((k, 1))
+    rank_mat = np.argsort(score_mat)
+    backup = np.copy(score_mat)
+    label_count = np.sum(true_mat, axis=1)
+
+    for m in range(k):
+        y_mat = np.copy(true_mat)
+        for i in range(rank_mat.shape[0]):
+            y_mat[i][rank_mat[i, :-(m + 1)]] = 0
+            for j in range(m + 1):
+                y_mat[i][rank_mat[i, -(j + 1)]] /= np.log(j + 1 + 1)
+
+        dcg = np.sum(y_mat, axis=1)
+        factor = get_factor(label_count, m + 1)
+        ndcg = np.mean(dcg / factor)
+        res[m] = ndcg
+    return np.around(res, decimals=4)
+    
+def get_factor(label_count,k):
+    res=[]
+    for i in range(len(label_count)):
+        n=int(min(label_count[i],k))
+        f=0.0
+        for j in range(1,n+1):
+            f+=1/np.log(j+1)
+        res.append(f)
+    return np.array(res)
